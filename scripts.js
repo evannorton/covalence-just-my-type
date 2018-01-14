d117bc130f06c3ade22b6af63d21fa85006a6df1
Evan Norton <evanan39@yahoo.com>
2018-01-14 17:24:34 -0500

    highlight inputted key

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 let $keyUpper = $("#keyboard-upper-container");
 let $keyLower = $("#keyboard-lower-container");
 
+//reads shift
 $(document).keydown(function (e1) {
-    let $shifted = e1.shiftKey;
-    if (e1.shiftKey) {
+    if (e1.which === 16) {
         $($keyUpper).css("display", "block");
         $($keyLower).css("display", "none");
-        console.log("shift pressed");
         $(document).keyup(function (e2) {
             console.log(e2.which);
             if (e2.which === 16) {
@@ -15,4 +14,14 @@ $(document).keydown(function (e1) {
             }
         });
     }
+});
+
+//reads keys
+$(document).keypress(function (e3) {
+    console.log(e3.which);
+    let $key = $("#" + e3.which);
+    $($key).css("background-color", "yellow");
+    $(document).keyup(function (e3) {
+        $($key).css("background-color", "#f5f5f5");
+    });
 });
