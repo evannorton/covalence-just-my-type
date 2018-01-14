82f780133a484cdbc0164910f8e697a04aa332b1
Evan Norton <evanan39@yahoo.com>
2018-01-14 18:30:46 -0500

    first sentence appears on screen, pressing correct key advances highlight

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 //vars
 let $playButton = $("#play-button");
 let $content = $("#target");
+let $highlight = $("#yellow-block");
+let $highlightPosition = 0;
 let $keyUpper = $("#keyboard-upper-container");
 let $keyLower = $("#keyboard-lower-container");
 let $sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-
+let $sentence = $sentences[0];
+let $letter = $sentence.substring(0, 1);
 
 //load page with button
 $($playButton).click(function () {
@@ -17,7 +20,6 @@ $($playButton).click(function () {
             $($keyUpper).css("display", "block");
             $($keyLower).css("display", "none");
             $(document).keyup(function (e2) {
-                console.log(e2.which);
                 if (e2.which === 16) {
                     $($keyUpper).css("display", "none");
                     $($keyLower).css("display", "block");
@@ -28,11 +30,19 @@ $($playButton).click(function () {
 
     //highlight keys
     $(document).keypress(function (e) {
-        console.log(e.which);
         let $key = $("#" + e.which);
         $($key).css("background-color", "yellow");
         $(document).keyup(function (e) {
             $($key).css("background-color", "#f5f5f5");
         });
+    });
+
+    $("#sentence").text($sentence);
+    $("#target-letter").text($letter);
+    $(document).keypress(function (e) {
+        if (e.which == $sentences[0].charCodeAt(0)) {
+            $highlightPosition += 20;
+            $($highlight).css("margin-left", $highlightPosition + "px");
+        }
     });
 });
