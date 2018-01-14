924cff1c1a3e59b1e2f396a30b92276813c4e134
Evan Norton <evanan39@yahoo.com>
2018-01-14 18:01:38 -0500

    load page with button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
+//vars
+let $playButton = $("#play-button");
+let $content = $("#target");
 let $keyUpper = $("#keyboard-upper-container");
 let $keyLower = $("#keyboard-lower-container");
+let $sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
-//reads shift
+
+//load page with button
+$($playButton).click( function () {
+    $($content).css("display", "block");
+    $($playButton).css("display", "none");
+});
+
+//toggle keyboards
 $(document).keydown(function (e1) {
     if (e1.which === 16) {
         $($keyUpper).css("display", "block");
@@ -16,12 +27,13 @@ $(document).keydown(function (e1) {
     }
 });
 
-//reads keys
-$(document).keypress(function (e3) {
-    console.log(e3.which);
-    let $key = $("#" + e3.which);
+//highlight keys
+$(document).keypress(function (e) {
+    console.log(e.which);
+    let $key = $("#" + e.which);
     $($key).css("background-color", "yellow");
-    $(document).keyup(function (e3) {
+    $(document).keyup(function (e) {
         $($key).css("background-color", "#f5f5f5");
     });
 });
+
